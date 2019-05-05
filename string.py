6eeae80bd9efe0c1f22816042b21a9c2c0bc1a1e
goulart <goulart@cubee.com>
2019-05-05 16:13:42 +0800

    add string utils

---
+++ string.py
@@ -0,0 +1,12 @@
+
+def sbc_to_dbc(sbc):
+    """Convert SBC case to DBC case"""
+    def convert(c):
+        ucode = ord(c)
+        if ucode == 12288:
+            return chr(32)
+        elif 65281 <= ucode <= 65374:
+            return chr(ucode - 65248)
+        else:
+            return c
+    return "".join([convert(c) for c in sbc])
